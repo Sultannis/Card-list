0e092c97342ba7d22b9bef087e827077ad944b21
Sultannis <sultan.fb29@gmail.com>
2020-07-19 01:09:53 +0600

    feat: replace header elements with ant design components

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -1,19 +1,25 @@
 <template>
   <header class="header">
-    <router-link class="header__btn" to="/create">Add card</router-link>
-    <div class="switch-box">
-      <h2 class="switch-box__title">Drag -n drop</h2>
-      <label class="switch">
-        <input type="checkbox" />
-        <span class="slider round"></span>
-      </label>
+    <router-link class="header__btn" to="/create">
+      <a-button type="primary">
+        Add card
+      </a-button></router-link
+    >
+    <div class="header__switch">
+      <h3 class="header__title">Drag -n drop</h3>
+      <a-switch default-checked @change="onChange" />
     </div>
   </header>
 </template>
 
 <script>
 export default {
-  name: "Header"
+  name: "Header",
+  methods: {
+    onChange(checked) {
+      console.log(`a-switch to ${checked}`);
+    }
+  }
 };
 </script>
 
@@ -25,76 +31,18 @@ export default {
   height: 60px;
   align-items: center;
   justify-content: space-between;
-}
 
-.switch-box {
-  display: flex;
-  align-items: center;
+  &__switch {
+    display: flex;
+    align-items: center;
+    height: 100%;
+  }
 
   &__title {
     font-size: 16px;
+    color: black;
+    margin: 0;
     margin-right: 10px;
   }
-}
-
-// Switch style
-
-.switch {
-  position: relative;
-  display: inline-block;
-  width: 30px;
-  height: 17px;
-}
-
-.switch input {
-  opacity: 0;
-  width: 0;
-  height: 0;
-}
-
-.slider {
-  position: absolute;
-  cursor: pointer;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  background-color: #ccc;
-  -webkit-transition: 0.4s;
-  transition: 0.4s;
-}
-
-.slider:before {
-  position: absolute;
-  content: "";
-  height: 13px;
-  width: 13px;
-  left: 2px;
-  bottom: 2px;
-  background-color: white;
-  -webkit-transition: 0.4s;
-  transition: 0.4s;
-}
-
-input:checked + .slider {
-  background-color: #2196f3;
-}
-
-input:focus + .slider {
-  box-shadow: 0 0 1px #2196f3;
-}
-
-input:checked + .slider:before {
-  -webkit-transform: translateX(13px);
-  -ms-transform: translateX(13px);
-  transform: translateX(13px);
-}
-
-.slider.round {
-  border-radius: 17px;
-}
-
-.slider.round:before {
-  border-radius: 50%;
 }
 </style>
