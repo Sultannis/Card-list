4194b616d9d3ec57063e60efe9b9ce813a4a87d9
Sultannis <sultan.fb29@gmail.com>
2020-07-19 01:23:33 +0600

    feat: create cards box basic markup

--- src/components/CardsBox/CardsBox.vue
+++ src/components/CardsBox/CardsBox.vue
@@ -1,13 +1,22 @@
 <template>
-  <h1>cardsBox</h1>
+  <div class="cards-box">
+    <div class="cards-box__column"></div>
+    <div class="cards-box__column"></div>
+    <div class="cards-box__column"></div>
+    <div class="cards-box__column"></div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: "CardsBox"
-}
+  name: "CardsBox"
+};
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+.cards-box {
+  width: 100%;
+  display: flex;
+  border: 1px rgb(203, 122, 235) solid;
+}
 </style>
