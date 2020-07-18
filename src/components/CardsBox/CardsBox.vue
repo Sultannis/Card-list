b5df0452b08e12e6c0b5d8b211fa963cef599813
Sultannis <sultan.fb29@gmail.com>
2020-07-19 01:17:56 +0600

    feat: create cards box component

---
+++ src/components/CardsBox/CardsBox.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>cardsBox</h1>
+</template>
+
+<script>
+export default {
+    name: "CardsBox"
+}
+</script>
+
+<style>
+
+</style>
--- src/views/List.vue
+++ src/views/List.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="home">
     <Header />
+    <CardsBox />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Header from "@/components/Header/Header.vue";
+import CardsBox from "@/components/CardsBox/CardsBox.vue";
 
 export default {
   name: "Home",
   components: {
-    Header
+    Header,
+    CardsBox
   }
 };
 </script>
