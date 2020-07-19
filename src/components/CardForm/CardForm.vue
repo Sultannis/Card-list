0dd254955388466794bfe9429f3873c2f59bf296
Sultannis <sultan.fb29@gmail.com>
2020-07-19 13:04:37 +0600

    feat: create card form component

---
+++ src/components/CardForm/CardForm.vue
@@ -0,0 +1,38 @@
+<template>
+  <a-form-model :model="card">
+    <a-form-model-item label="Card Title">
+      <a-input v-model="card.title" />
+    </a-form-model-item>
+    <a-form-model-item label="Activity form">
+      <a-input v-model="card.desc" type="textarea" />
+    </a-form-model-item>
+    <a-form-model-item>
+      <a-button type="primary" @click="onSubmit">
+        Create
+      </a-button>
+      <router-link @click="(card.title = ''), (card.desc = '')" to="/">
+        <a-button style="margin-left: 10px;">
+          Cancel
+        </a-button>
+      </router-link>
+    </a-form-model-item>
+  </a-form-model>
+</template>
+
+<script>
+export default {
+  name: "CardForm",
+  data() {
+    return {
+      labelCol: { span: 4 },
+      wrapperCol: { span: 14 },
+      card: {
+        title: "",
+        desc: ""
+      }
+    };
+  }
+};
+</script>
+
+<style></style>
--- src/views/CreateCard.vue
+++ src/views/CreateCard.vue
@@ -1,41 +1,17 @@
 <template>
   <div class="form">
     <h2 class="form__title">Create new card</h2>
-    <a-form-model :model="card">
-      <a-form-model-item label="Card Title">
-        <a-input v-model="card.title" />
-      </a-form-model-item>
-      <a-form-model-item label="Activity form">
-        <a-input v-model="card.desc" type="textarea" />
-      </a-form-model-item>
-      <a-form-model-item>
-        <a-button type="primary" @click="onSubmit">
-          Create
-        </a-button>
-        <router-link @click="(card.title = ''), (card.desc = '')" to="/">
-          <a-button style="margin-left: 10px;">
-            Cancel
-          </a-button>
-        </router-link>
-      </a-form-model-item>
-    </a-form-model>
+    <CardForm />
   </div>
 </template>
 <script>
-import cardForm from "vuex";
+import CardForm from "@/components/CardForm/CardForm";
 
 export default {
-  data() {
-    return {
-      labelCol: { span: 4 },
-      wrapperCol: { span: 14 },
-      card: {
-        title: "",
-        desc: ""
-      }
-    };
+  components: {
+    CardForm
   },
-  mutations: {},
+  mutations: {}
   // methods: {
   //   onSubmit() {
   //     this.$store.commit("onSubmit(this.card)");
